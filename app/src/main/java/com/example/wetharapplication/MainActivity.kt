56a464ec04a13051586e78474981a7fa697d2d01
milad <miladsoliman55@gmail.com>
2023-03-20 00:36:23 +0200

    Navigation Drawer Design

--- app/src/main/java/com/example/wetharapplication/MainActivity.kt
+++ app/src/main/java/com/example/wetharapplication/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.example.wetharapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.navigateUp
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var navController: NavController
+    lateinit var navigationview: NavigationView
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var appBarConfigartion: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = findNavController(R.id.nav_host_fragment)
+     //   navController = findNavController(this, R.id.nav_host_fragment)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        appBarConfigartion = AppBarConfiguration(navController.graph , drawerLayout)
+        navigationview = findViewById(R.id.navigationView)
+        setupWithNavController(navigationview,navController)
+
+        val actionBar = supportActionBar
+        actionBar?.setHomeAsUpIndicator(R.drawable.baseline_menu_24)
+        actionBar!!.setDisplayShowHomeEnabled(true)
+        actionBar!!.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+   /* override fun onSupportNavigateUp(): Boolean {
+        var navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfigartion)  || super.onSupportNavigateUp()
+
+    }*/
+   override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START)
+            } else {
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
+
